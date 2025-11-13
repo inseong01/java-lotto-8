2466fd5d16f3a3b5013664d9aaaa3c1bfd3d4f3f
inseong01 <insung940@naver.com>
2025-11-13 10:49:06 +0900

    feat: 로또 클래스 구현
    - 로또 기능 구현
    - 테스트 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,32 @@
 package lotto;
 
+import lotto.utility.Validator;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+  private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+  public Lotto(List<Integer> numbers) {
+      validate(numbers);
+      this.numbers = numbers;
+  }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+  private void validate(List<Integer> numbers) {
+    Validator.lottoValidate(numbers);
+  }
 
-    // TODO: 추가 기능 구현
+  public List<Integer> getNumbers() {
+      return numbers;
+  }
+
+  public  int getMatchCountOf(List<Integer> jackpotNumbers) {
+
+    return numbers.stream().reduce(0, (subTotal, num) -> {
+      if (jackpotNumbers.contains(num)) {
+        return subTotal + 1;
+      }
+      return subTotal;
+    });
+  }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,21 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+  @Test
+  void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+      assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+              .isInstanceOf(IllegalArgumentException.class);
+  }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+  @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+  @Test
+  void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+      assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+              .isInstanceOf(IllegalArgumentException.class);
+  }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  @Test
+  void 메서드를_통해서_로또_번호를_반환한다() {
+    Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    List<Integer> numbers = lotto.getNumbers();
+
+    assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+  }
+
+  @Test
+  void 메서드를_통해서_로또_번호_일치_개수를_반환한다() {
+    List<Integer> JACKPOT_LOTTO_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
+    List<Integer> MY_LOTTO_NUMBERS = List.of(1, 12, 33, 41, 5, 6);
+
+    Lotto lotto = new Lotto(MY_LOTTO_NUMBERS);
+
+    assertThat(lotto.getMatchCountOf(JACKPOT_LOTTO_NUMBERS)).isEqualTo(3);
+  }
 }
