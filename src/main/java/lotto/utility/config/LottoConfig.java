c42d2b356350a942b48e32b977b147f498e5e7bb
inseong01 <insung940@naver.com>
2025-11-14 12:30:36 +0900

    feat: LottoConfig enum 값 추가
    - 로또 번호 생성 범위
    - 로또 번호 개수

--- src/main/java/lotto/utility/config/LottoConfig.java
+++ src/main/java/lotto/utility/config/LottoConfig.java
@@ -1,7 +1,10 @@
 package lotto.utility.config;
 
 public enum LottoConfig {
-  LOTTO_PRICE(1_000);
+  LOTTO_PRICE(1_000),
+  NUMBER_START_RANGE(1),
+  NUMBER_END_RANGE(45),
+  NUMBER_AMOUNT(6);
 
   private final int value;
 
