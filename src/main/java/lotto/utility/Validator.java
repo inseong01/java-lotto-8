93bb3b267f1dc096658266e7f0f46958714f581c
inseong01 <insung940@naver.com>
2025-11-14 10:49:42 +0900

    refactor: 검증 클래스 상수 파일 적용
    - 오류 메시지 문자열 -> 메서드 적용

--- src/main/java/lotto/utility/Validator.java
+++ src/main/java/lotto/utility/Validator.java
@@ -1,21 +1,27 @@
 package lotto.utility;
 
-import net.bytebuddy.implementation.bytecode.Throw;
+import lotto.utility.message.BonusMessage;
+import lotto.utility.message.CashMessage;
+import lotto.utility.message.InputMessage;
+import lotto.utility.message.LottoMessage;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validator {
   public static void inputValidate(String input) {
     if (input.trim().isEmpty()) {
-      throw new IllegalArgumentException("[ERROR] 다시 입력해주세요.");
+      throw new IllegalArgumentException(
+              InputMessage.INPUT_IS_EMPTY_STRING.getDescription()
+      );
     }
 
     Pattern noneNumberPattern = Pattern.compile("\\D");
 
     if (noneNumberPattern.matcher(input).find()) {
-      throw new IllegalArgumentException("[ERROR] 양의 정수로 입력해주세요.");
+      throw new IllegalArgumentException(
+              InputMessage.INPUT_IS_NOT_A_NUMBER.getDescription()
+      );
     }
   }
 
@@ -23,37 +29,49 @@ public class Validator {
     int PRODUCE_PRICE = 1_000;
 
     if (cash % PRODUCE_PRICE != 0) {
-      String message = String.format("[ERROR] %d원 단위로 작성해주세요.", PRODUCE_PRICE);
-      throw new IllegalArgumentException(message);
+      throw new IllegalArgumentException(
+              CashMessage.CASH_HAS_CHANGE.format()
+      );
     }
 
     if (cash == 0) {
-      String message = String.format("[ERROR] %d원 이상으로 작성해주세요.", PRODUCE_PRICE);
-      throw new IllegalArgumentException(message);
+      throw new IllegalArgumentException(
+              CashMessage.CASH_IS_ZERO.format()
+      );
     }
   }
 
   public static void lottoValidate(List<Integer> numbers) {
     if (numbers.size() != 6) {
-      throw new IllegalArgumentException("[ERROR] 로또 숫자는 6개이어야 합니다.");
+      throw new IllegalArgumentException(
+              LottoMessage.NUMBER_LENGTH_IS_WRONG.getDescription()
+      );
     }
 
     if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위 내 숫자로 구성해야 합니다.");
+      throw new IllegalArgumentException(
+              LottoMessage.NUMBER_IS_OUT_OF_RANGE.getDescription()
+      );
     }
 
     if (numbers.stream().distinct().count() != 6) {
-      throw new IllegalArgumentException("[ERROR] 숫자는 중복되어서는 안 됩니다.");
+      throw new IllegalArgumentException(
+              LottoMessage.NUMBER_IS_DUPLICATED.getDescription()
+      );
     }
   }
 
   public static void bonusValidate(int number, List<Integer> lottoNumbers) {
     if (number < 1 || number > 45) {
-      throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 범위 내 숫자로 구성해야 합니다.");
+      throw new IllegalArgumentException(
+              BonusMessage.NUMBER_IS_OUT_OF_RANGE.getDescription()
+      );
     }
 
     if (lottoNumbers.stream().anyMatch(num -> num == number)) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복되어서는 안 됩니다.");
+      throw new IllegalArgumentException(
+              BonusMessage.NUMBER_IS_DUPLICATED.getDescription()
+      );
     }
   }
 }
