d644dcfa1ca8082cd744cb64b475a5f52115d033
inseong01 <insung940@naver.com>
2025-11-16 09:19:21 +0900

    feat: 로또 구현
    - 로또 컨트롤러 메서드 배치
    - 예제 테스트 통과

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(String[] args) {
+    LottoController lottoController = new LottoController();
+
+    int cash = lottoController.getUserCashInput();
+    lottoController.setCash(cash);
+
+    lottoController.setUserLotto();
+
+    lottoController.printUserLotto();
+
+    List<Integer> numbers = lottoController.getLottoNumbers();
+    lottoController.setJackpotLotto(numbers);
+
+    int number = lottoController.getBonusNumber();
+    lottoController.setBonusNumber(number);
+
+    lottoController.printLottoResult();
+  }
 }
